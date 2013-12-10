8cbe44220533581f4d3b2aa08c5b71d97fe33320
Anna McKelvey <anna.e.mckelvey@gmail.com>
2013-12-09 22:17:52 -0600

    Added controllers and views according to framework notes

--- config/config.php
+++ config/config.php
@@ -14,7 +14,7 @@ If you want a different default time format for this app, set it below
 */
 
 # What is the name of this app?
-	define('APP_NAME', 'Sample App');  
+	define('APP_NAME', 'GlassFaucet');  
 
 # When email is sent out from the server, where should it come from?
 # Ideally, this should match the domain name
@@ -31,7 +31,7 @@ A email designated to receive messages from the server. Examples:
 	define('SYSTEM_EMAIL', 'webmaster@myapp.com'); 
 
 # Default DB name for this app
-	define('DB_NAME', 'myapp_com'); 
+	define('DB_NAME', 'japannab_p4_japanna_me'); 
 
 # Timezone
 	define('TIMEZONE', 'America/New_York');
@@ -43,5 +43,5 @@ A email designated to receive messages from the server. Examples:
 	//define('SMTP_PASSWORD', '');
 
 # For extra security, you might want to set different salts than what the core uses
-	//define('PASSWORD_SALT', '');
-	//define('TOKEN_SALT', '');
+	define('PASSWORD_SALT', 'tyc99jPzswbhRqg8lMb)qeF7AJUhz36KVj^a60t_');
+	define('TOKEN_SALT', 'VCpL3op*-h6y6T@C(E)kH9Zd%49GwheSAbMfr^7D');
---
+++ views/v_users_profile.php
@@ -0,0 +1,1 @@
+<h1>This is the profile of <?=$user->name?></h1>
