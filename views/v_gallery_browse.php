561fccf849d3ffec1e9290f6c5440e46d94d7caf
Anna McKelvey <anna.e.mckelvey@gmail.com>
2013-12-12 10:47:13 -0600

    Added links to detaild item page from favorites. Removed 2nd image from gallery and favorites

--- views/v_gallery_browse.php
+++ views/v_gallery_browse.php
@@ -3,7 +3,6 @@
 	<?php foreach($posts as $post): ?>
 	<p>
 		<a href="/gallery/item/<?=$post['serial_no']?>"><img class="img_front" src="/uploads/faucets/<?=$post['img_front']?>" alt="<?=$post['serial_no']?> <?=$post['color']?> front"></a>
-    	<a href="/gallery/item/<?=$post['serial_no']?>"><img class="img_side" src="/uploads/faucets/<?=$post['img_side']?>" alt="<?=$post['serial_no']?> <?=$post['color']?> side"></a>
     	<h1><?=$post['color']?></h1>
     	<h2><?=$post['serial_no']?></h2>
     	<p>Overall height: <?=$post['overall_height_in']?> (in) / <?=$post['overall_height_cm']?> (cm)</p>
