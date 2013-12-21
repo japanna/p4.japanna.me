be2d908dcce663074b7c081d4cd14f75e6916fc7
Anna McKelvey <anna.e.mckelvey@gmail.com>
2013-12-20 22:43:41 -0600

    fixed active nav js for login & signup

--- js/signup.js
+++ js/signup.js
@@ -5,5 +5,5 @@ Marks active nav
 $(document).ready(function(){
 	$('#filter_list').hide();
 	$('.back_to_gallery').hide();
-	$('#nav_right').children().eq(5).addClass('active');
+	$('#nav_right').children().eq(4).addClass('active');
 });
